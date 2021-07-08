b816946da22847f91dabb31d64ffc0d4e3cf30fe
Daud <abubakar.ranglerz@gmail.com>
2021-07-09 00:03:20 +0500

    daud

--- app/Http/Controllers/APIController.php
+++ app/Http/Controllers/APIController.php
@@ -68,7 +68,7 @@ class APIController extends Controller
 		return json_encode($response);
 	}
 
-	public function login(Request $request){
+	public function login(Request $request){ 
 		$response = (object) null;
 		if(!isset($request->number)){
 			$status = 400;
@@ -133,5 +133,94 @@ class APIController extends Controller
 		$response->message = 'Successfully Updated';
 		$response->data = DB::table('clients')->where('id',$request->id)->first();
 		return json_encode($response);
+	}
+
+	public function get_location(Request $request){
+		$response = (object) null;
+		if(!isset($request->location)){
+			$response->status = 400;
+			$response->message = 'Location is required';
+			$response->data = null;
+			return json_encode($response);
+		}
+
+		else{
+			$address = $request->location; // Address
+		        $apiKey = 'AIzaSyAd8q-fqcHslANRJ3WZxR5cMYY1CgtBe9I'; // Google maps now requires an API key.
+		        // Get JSON results from this request
+		       
+		        $geo = file_get_contents('https://maps.googleapis.com/maps/api/geocode/json?address='.urlencode($address).'&sensor=false&key='.$apiKey);
+		       
+		        $geo = json_decode($geo, true); // Convert the JSON to an array
+		        if (isset($geo['status']) && ($geo['status'] == 'OK')) {
+		          $latitude = $geo['results'][0]['geometry']['location']['lat']; // Latitude
+		          $longitude = $geo['results'][0]['geometry']['location']['lng']; // Longitude
+		          
+		          $data = array('location'=> $request->location,'latitude' => $latitude,'longitude' => $longitude);
+		          	$response->status = 200;
+					$response->message = 'Success';
+					$response->data = $data;
+					return json_encode($response);
+		        }
+		       return 'Location not Valid';
+		    }
+	}
+
+	public function dropoff_location(Request $request){
+
+	}
+
+	public function pickup_location(Request $request){
+
+	}
+
+	public function get_available_vehicles(Request $request){
+
+	}
+
+	public function choose_vehicle(Request $request){
+		$response = (object) null;
+		if(!isset($request->vehicle_id)){
+			$response->status = 400;
+			$response->message = 'Vehicle Id is required';
+			$response->data = null;
+			return json_encode($response);
+		}
+
+		else{
+			// send drivers list
+		}
+	}
+
+	public function select_driver(Request $request){
+		$response = (object) null;
+		if(!isset($request->driver_id)){
+			$response->status = 400;
+			$response->message = 'Driver Id is required';
+			$response->data = null;
+			return json_encode($response);
+		}
+
+		else{
+			// send fare rates
+		}
+	}
+
+	public function booking_confirmation(Request $request){
+		$response = (object) null;
+		if(!isset($request->driver_id)){
+			$response->status = 400;
+			$response->message = 'Driver Id is required';
+			$response->data = null;
+			return json_encode($response);
+		}
+
+		else{
+			// send fare rates
+		}
+	}
+
+	public function booking_history(Request $request){
+
 	}
 }
--- routes/api.php
+++ routes/api.php
@@ -22,3 +22,15 @@ Route::middleware('auth:api')->get('/user', function (Request $request) {
 Route::post('sign_up', '\App\Http\Controllers\APIController@sign_up');
 Route::post('login','\App\Http\Controllers\APIController@login');
 Route::post('profile_update','\App\Http\Controllers\APIController@profile_update');
+
+// location 
+Route::post('get_location','\App\Http\Controllers\APIController@get_location');
+Route::post('dropoff_location','\App\Http\Controllers\APIController@dropoff_location');
+Route::post('pickup_location','\App\Http\Controllers\APIController@pickup_location');
+
+// Vehicles
+Route::post('get_available_vehicles','\App\Http\Controllers\APIController@get_available_vehicles');
+Route::post('choose_vehicle','\App\Http\Controllers\APIController@choose_vehiclse');
+Route::post('select_driver','\App\Http\Controllers\APIController@select_driver');
+Route::post('booking_confirmation','\App\Http\Controllers\APIController@booking_confirmation');
+Route::post('booking_history','\App\Http\Controllers\APIController@booking_history');
